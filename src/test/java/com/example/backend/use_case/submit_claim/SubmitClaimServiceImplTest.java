ee7cf478dd2fe5aa2a5621531498c208537e5a40
Sarva Sanjay <sarva.sanjay@mail.utoronto.ca>
2023-12-03 18:24:22 -0500

    test submitclaim added

---
+++ src/test/java/com/example/backend/use_case/submit_claim/SubmitClaimServiceImplTest.java
@@ -0,0 +1,70 @@
+package com.example.backend.use_case.submit_claim;
+
+import com.example.backend.entities.Claim;
+import com.example.backend.repositories.ClaimRepository;
+import com.example.backend.responsemodel.CommonResponse;
+import com.example.backend.responsemodel.GenericException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class SubmitClaimServiceImplTest {
+
+    @Mock
+    private ClaimRepository claimRepository;
+
+    @InjectMocks
+    private SubmitClaimServiceImpl submitClaimService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testSubmitClaim_Success() {
+        // Mock data
+        String claimNumber = "123";
+        Claim claim = new Claim();
+        claim.setClaimNumber(claimNumber);
+
+        // Stubbing repository method
+        when(claimRepository.findByClaimNumber(claimNumber)).thenReturn(claim);
+        when(claimRepository.save(any())).thenReturn(claim);
+
+        // Perform the action
+        CommonResponse<String> response = submitClaimService.submitClaim(claimNumber);
+
+        // Verify interactions and assertions
+        verify(claimRepository, times(1)).findByClaimNumber(claimNumber);
+        verify(claimRepository, times(1)).save(claim);
+        assertAll(
+                () -> assertEquals(200, response.getStatus()),
+                () -> assertEquals("Claim submitted successfully", response.getMessage())
+        );
+    }
+
+    @Test
+    void testSubmitClaim_ClaimNotFound() {
+        // Mock data
+        String claimNumber = "456";
+
+        // Stubbing repository method
+        when(claimRepository.findByClaimNumber(claimNumber)).thenReturn(null);
+
+        // Perform the action and expect an exception
+        assertThrows(GenericException.class, () -> submitClaimService.submitClaim(claimNumber));
+
+        // Verify interactions
+        verify(claimRepository, times(1)).findByClaimNumber(claimNumber);
+        verify(claimRepository, never()).save(any());
+    }
+
+    // Add more test cases as needed
+}
